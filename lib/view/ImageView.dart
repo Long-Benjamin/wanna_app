c4edf0b23a31c25594c1bd47569a5d79dcca805e
longj <longjintang123@163.com>
2020-05-05 12:51:38 +0800

    【1.0】类似ImageView展示图片

--- lib/view/ImageView.dart
+++ lib/view/ImageView.dart
@@ -16,8 +16,33 @@ class _ImageViewPageState extends State<ImageViewPage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Image(
+              image: AssetImage("images/ic_place.jpg"),
+                width: 240.0,
+            ),
+
+            Image.asset("images/ic_person.jpg",
+              height: 280.0,
+              color: Colors.purple[600],
+              colorBlendMode: BlendMode.colorDodge,
+            ),
+
+            Image(
+              image: NetworkImage("http://img5.mtime.cn/mg/2016/10/02/160406.41155838.jpg"),
+            ),
+
+            Icon(Icons.fingerprint, color: Colors.red, size: 54.0),
+
+            Image(
+              image: NetworkImage("http://img5.mtime.cn/mg/2016/10/02/160406.41155838.jpg"),
+              color: Colors.blue,
+              colorBlendMode: BlendMode.plus,
+              height: 240,
+            ),
+
+          ],),
         ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
