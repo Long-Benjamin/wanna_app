c2a387c7c2fc2f0369503ac472515bd56dc9a5ec
longj <longjintang123@163.com>
2020-05-05 21:33:30 +0800

    【1.0】Linearlayout位置排列

--- lib/view/LinearLayout.dart
+++ lib/view/LinearLayout.dart
@@ -16,8 +16,72 @@ class _LinearLayoutPageState extends State<LinearLayoutPage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(),
+        body: Container(
+          child: Column(
+            //测试Row对齐方式，排除Column默认居中对齐的干扰
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("垂直布局，列0", style: TextStyle(backgroundColor: Colors.grey),),
+              Text("垂直布局，列1"),
+              Text("垂直布局，列2"),
+              Image.asset("images/ic_place.jpg",scale: 20,),
+
+              const SizedBox(height: 10,),
+
+              Wrap(
+                spacing: 8.0, // 主轴(水平)方向间距
+                runSpacing: 4.0, // 纵轴（垂直）方向间距
+                alignment: WrapAlignment.start, //沿主轴方向居中
+                children: <Widget>[
+                  new Chip(
+                    avatar: new CircleAvatar(backgroundColor: Colors.blue, child: Text('A')),
+                    label: new Text('HamiltonItTooLong'),
+                  ),
+                  new Chip(
+                    avatar: new CircleAvatar(backgroundColor: Colors.blue, child: Text('M')),
+                    label: new Text('Lafayette'),
+                  ),
+                  new Chip(
+                    avatar: new CircleAvatar(backgroundColor: Colors.blue, child: Text('H')),
+                    label: new Text('Mulligan'),
+                  ),
+                  new Chip(
+                    avatar: new CircleAvatar(backgroundColor: Colors.blue, child: Text('J')),
+                    label: new Text('Laurens'),
+                  ),
+                  new Chip(
+                    avatar: new CircleAvatar(backgroundColor: Colors.blue, child: Text('T')),
+                    label: new Text('Jintang'),
+                  ),
+
+                ],
+              ),
+
+              const SizedBox(height: 10,),
+
+              SizedBox(
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Image.asset("images/ic_jiche.jpg" ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Image.asset("images/ic_jiche.jpg" ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Image.asset("images/ic_jiche.jpg" ),
+                    ),
+                  ],
+              ),
+              )
+
+
+            ],
+          ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
