fdd78989eb9057ea20c38742caed8e5d58cc326c
longj <longjintang123@163.com>
2020-05-05 22:40:14 +0800

    【1.0】使用三种简单的弹窗

--- lib/view/Dialog.dart
+++ lib/view/Dialog.dart
@@ -17,8 +17,90 @@ class _DialogPageState extends State<DialogPage> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(),
+          child: Column(children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                showAboutDialog(context: context,
+                    applicationName: "Wanna App",
+                    applicationVersion: "v1.0",
+                    applicationIcon: FlutterLogo(),
+                    applicationLegalese: "Copyright 2003-2015, The Flying Monkee, LLC (excepting graphics, as freeware, licensed, and/or in public domain). All rights reserved. ",
+                    children: null,
+                    useRootNavigator: false);
+              },
+              textColor: Colors.white,
+              color: Colors.lightBlue,
+              child: Text("关于APP弹窗"),
+            ),
+
+            RaisedButton(
+              onPressed: () {
+                showDeleteConfirmDialog();
+              },
+              textColor: Colors.white,
+              color: Colors.lightBlue,
+              child: Text("普通对话框"),
+            ),
+
+            RaisedButton(
+              onPressed: () {
+                showLoadingDialog();
+              },
+              textColor: Colors.white,
+              color: Colors.lightBlue,
+              child: Text("普通Loading框"),
+            ),
+
+          ],),
         ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  // 弹出对话框
+  Future<bool> showDeleteConfirmDialog() {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("提示"),
+          content: Text("您已经点击了这个按钮?"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("取消"),
+              onPressed: () => Navigator.of(context).pop(), // 关闭对话框
+            ),
+            FlatButton(
+              child: Text("删除"),
+              onPressed: () {
+                //关闭对话框并返回true
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  showLoadingDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false, //点击遮罩不关闭对话框
+      builder: (context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              CircularProgressIndicator(),
+              Padding(
+                padding: const EdgeInsets.only(top: 26.0),
+                child: Text("正在加载，请稍后..."),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
 }
